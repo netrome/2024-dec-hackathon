26744dc2c8136c0268c00c5f77efe0fbd676a11e
Mårten Blankfors <marten@blankfors.se>
2024-12-20 16:08:46 +0100

    feat: Can fulfill claims

--- kpop-app/src/comp/claims.rs
+++ kpop-app/src/comp/claims.rs
@@ -1,4 +1,4 @@
-use leptos::prelude::*;
+use leptos::{either::Either, prelude::*};
 
 use crate::model;
 
@@ -19,7 +19,7 @@ pub fn claims() -> impl IntoView {
                     {move || {
                         claims_on_user
                             .get()
-                            .map(|res| res.map(|claims| view! { <ClaimsTable claims /> }))
+                            .map(|res| res.map(|claims| view! { <ClaimsTable claims is_user=false /> }))
                     }}
                 </article>
                 <article>
@@ -29,7 +29,7 @@ pub fn claims() -> impl IntoView {
                     {move || {
                         user_claims
                             .get()
-                            .map(|res| res.map(|claims| view! { <ClaimsTable claims /> }))
+                            .map(|res| res.map(|claims| view! { <ClaimsTable claims is_user=true /> }))
                     }}
                 </article>
             </ErrorBoundary>
@@ -38,7 +38,7 @@ pub fn claims() -> impl IntoView {
 }
 
 #[component]
-pub fn claims_table(claims: Vec<model::Claim>) -> impl IntoView {
+pub fn claims_table(claims: Vec<model::Claim>, is_user: bool) -> impl IntoView {
     let (active_idx, set_active_idx) = signal(usize::MAX);
 
     let claim_rows = move || -> Vec<_> {
@@ -48,7 +48,11 @@ pub fn claims_table(claims: Vec<model::Claim>) -> impl IntoView {
             .enumerate()
             .map(|(idx, claim)| {
                 let is_active = active_idx.get() == idx;
-                view! { <ClaimRow claim idx is_active set_active_idx /> }
+                if is_user {
+                    Either::Left(view! { <InboundClaimRow claim idx is_active set_active_idx /> })
+                } else {
+                    Either::Right(view! { <OutboundClaimRow claim idx is_active set_active_idx /> })
+                }
             })
             .collect()
     };
@@ -72,7 +76,7 @@ pub fn claims_table(claims: Vec<model::Claim>) -> impl IntoView {
 }
 
 #[component]
-pub fn claim_row(
+pub fn outbound_claim_row(
     claim: model::Claim,
     idx: usize,
     is_active: bool,
@@ -111,6 +115,46 @@ pub fn claim_row(
     }
 }
 
+#[component]
+pub fn inbound_claim_row(
+    claim: model::Claim,
+    idx: usize,
+    is_active: bool,
+    set_active_idx: WriteSignal<usize>,
+) -> impl IntoView {
+    let toggle_active = move |_| {
+        if is_active {
+            set_active_idx.set(usize::MAX);
+        } else {
+            set_active_idx.set(idx);
+        }
+    };
+
+    let fulfill_action = ServerAction::<FulfillClaim>::new();
+
+    let fulfill_button = if is_active {
+        Some(view! {
+            <button on:click=move |_| {
+                fulfill_action.dispatch(claim.claim_id.into());
+            }>"fulfill"</button>
+        })
+    } else {
+        None
+    };
+
+    view! {
+        <tr on:click=toggle_active>
+            <td>{fulfill_button}</td>
+            <td>{claim.claim_id}</td>
+            <td>{claim.block_height}</td>
+            <td>{claim.asset_id}</td>
+            <td>{claim.amount}</td>
+            <td>{claim.recipient}</td>
+            <td>{claim.owner}</td>
+        </tr>
+    }
+}
+
 #[server]
 async fn get_claims() -> Result<Vec<model::Claim>, ServerFnError> {
     use crate::server::KpopServer;
@@ -133,6 +177,16 @@ async fn disprove_claim(id: u64) -> Result<(), ServerFnError> {
     let kp: KpopServer = use_context().expect("should be able to get shared Kpop instance");
 
     kp.disprove_claim(id).await;
+
+    Ok(())
+}
+
+#[server]
+async fn fulfill_claim(id: u64) -> Result<(), ServerFnError> {
+    use crate::server::KpopServer;
+    let kp: KpopServer = use_context().expect("should be able to get shared Kpop instance");
+
+    kp.fulfill_claim(id).await;
 
     Ok(())
 }
